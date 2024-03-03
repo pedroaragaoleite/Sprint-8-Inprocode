c815b66613247d75f806b889ba9eb1dc6a7273fc
Pedro Aragão <pedro.aragao.leite@gmail.com>
2024-03-03 10:57:29 +0100

    teste

--- frontend/src/app/services/database/database.service.ts
+++ frontend/src/app/services/database/database.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { EventsData } from '../../interfaces/eventsData';
 
-const apiSql = 'localhost://api'
+const apiSql = 'localhost://api';
 
 @Injectable({
   providedIn: 'root'
