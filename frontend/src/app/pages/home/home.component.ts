73b3608d440be37228514e7b42b549e364837247
Pedro Aragão <pedro.aragao.leite@gmail.com>
2024-03-03 10:01:09 +0100

    add mysql2, css style table and configure mysqldatabase and server

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMapLocation } from '@fortawesome/free-solid-svg-icons';
 
+
+
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -12,5 +15,17 @@ import { faMapLocation } from '@fortawesome/free-solid-svg-icons';
 export class HomeComponent {
 
   faMapLocation: any = faMapLocation;
+
+  tableRows: any = document.getElementsByTagName('tbody');
+  // rows: any = this.tableRows.getElementsByTagName('td');
+
+  constructor() {
+    console.log(this.tableRows);
+
+    // console.log(this.rows.length);
+
+  }
+
+
 
 }
