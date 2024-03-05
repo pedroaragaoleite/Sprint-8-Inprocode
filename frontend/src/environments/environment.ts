e75b859b5b87035eb259bf2510b86678fa84fd5f
Pedro Aragão <pedro.aragao.leite@gmail.com>
2024-03-05 22:13:30 +0100

    Delete frontend/src/environments directory

--- frontend/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    mapboxAccessToken: 'pk.eyJ1IjoicGVkcm9hcmFnYW9sZWl0ZSIsImEiOiJjbHRhZ3M5bW4wcnhoMmpxZmR4cWQyZnE2In0.fTPr2cqo7cM3UqKr0FWcog'
-};
